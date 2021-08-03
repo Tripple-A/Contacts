f628b97a9550a93087e7083b174712859f48376c
BIODUN <biodun9@gmail.com>
2021-08-03 18:06:45 +0100

    dockerized the app

--- contact-api/db/schema.rb
+++ contact-api/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_07_27_111422) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "audits", force: :cascade do |t|
     t.integer "auditable_id"
     t.string "auditable_type"
